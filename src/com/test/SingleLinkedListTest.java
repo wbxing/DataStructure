346c8bbf4a8fefafbb266a34ee3ca26deb515bf7
wbxing <wenbiaoxing@gmail.com>
2019-10-06 22:28:57 +0800

    单向链表

---
+++ src/com/test/SingleLinkedListTest.java
@@ -0,0 +1,33 @@
+package com.test;
+
+import com.data.structure.SingleLinkedList;
+
+public class SingleLinkedListTest {
+    public static void main(String[] args) {
+        SingleLinkedList singleList = new SingleLinkedList();
+        // 打印空链表
+        singleList.display();
+        // t头插法
+        singleList.addHead("B");
+        singleList.display();
+
+        singleList.addHead("A");
+        singleList.display();
+
+        // 尾插法
+        singleList.addTail("C");
+        singleList.display();
+
+        singleList.addTail("D");
+        singleList.display();
+
+        //删除
+        System.out.println(singleList.delete("D"));
+        singleList.display();
+        //查找
+        System.out.println(singleList.find("D"));
+        // 删除头节点
+        System.out.println(singleList.deleteHead());
+        singleList.display();
+    }
+}
