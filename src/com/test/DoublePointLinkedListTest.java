6805cc9157b953790d8f0847cc184040df209acd
wbxing <wenbiaoxing@gmail.com>
2019-10-08 20:37:16 +0800

    双端链表

---
+++ src/com/test/DoublePointLinkedListTest.java
@@ -0,0 +1,30 @@
+package com.test;
+
+import com.data.structure.DoublePointLinkedList;
+
+import java.util.DoubleSummaryStatistics;
+
+public class DoublePointLinkedListTest {
+    public static void main(String[] args) {
+        DoublePointLinkedList dpl = new DoublePointLinkedList();
+        // 打印空链表
+        dpl.display();
+        // 头插法, 默认添加方法
+        dpl.add("B");
+        dpl.display();
+
+        dpl.add("A");
+        dpl.display();
+
+        // 尾插法
+        dpl.add("C", "tail");
+        dpl.display();
+
+        dpl.add("D", "tail");
+        dpl.display();
+
+        // 删除头节点
+        System.out.println(dpl.deleteHead());
+        dpl.display();
+    }
+}
