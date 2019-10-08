db0aa0c5c812c197f5f4a944ce346562a129cb3e
wbxing <wenbiaoxing@gmail.com>
2019-10-08 21:23:16 +0800

    双端链表实现队列

---
+++ src/com/data/structure/QueueLinkedList.java
@@ -0,0 +1,39 @@
+package com.data.structure;
+
+/**
+ * 双端链表实现队列
+ * 实现功能
+ * 入队：push
+ * 出队：pop
+ * 判空：isEmpty
+ * 队列长度：getSize
+ * 打印队列：display
+ */
+public class QueueLinkedList {
+    private DoublePointLinkedList dp;
+
+    public QueueLinkedList() {
+        this.dp = new DoublePointLinkedList();
+    }
+
+    // 尾插法入队
+    public void push(Object obj) {
+        this.dp.add(obj, "tail");
+    }
+
+    public Object pop() {
+        return this.dp.deleteHead();
+    }
+
+    public boolean isEmpty() {
+        return this.dp.isEmpty();
+    }
+
+    public int getSize() {
+        return this.dp.getSize();
+    }
+
+    public void display() {
+        this.dp.display();
+    }
+}
