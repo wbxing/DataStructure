896fc2b41dba0214b7c0a61fec4cd8fe34468236
wbxing <wenbiaoxing@gmail.com>
2019-10-15 22:41:33 +0800

    简单队列 = 测试

---
+++ src/com/test/TestMyQueue.java
@@ -0,0 +1,16 @@
+package com.test;
+
+import com.data.structure.MyQueue;
+
+public class TestMyQueue {
+    public static void main(String[] args) {
+        MyQueue queue = new MyQueue(3);
+        queue.push(1);
+        queue.push(2);
+        queue.push(3);
+        System.out.println(queue.peek());
+        System.out.println(queue.pop());
+        queue.push(4);
+        queue.push(5);
+    }
+}
