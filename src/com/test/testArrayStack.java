5696c8bb1627a9c1f8e17698a5dab547602963ac
wbxing <wenbiaoxing@gmail.com>
2019-10-15 19:36:39 +0800

    栈的实现 - 测试 - 字符串反转

--- src/com/test/testArrayStack.java
+++ src/com/test/testArrayStack.java
@@ -22,5 +22,20 @@ public class testArrayStack {
 
         System.out.println(stack.peek());
 
+        // 字符串反转
+        stringReversal("Hello World!");
+    }
+
+    // 利用栈实现字符串反转
+    public static void stringReversal(String s) {
+        ArrayStack stack = new ArrayStack();
+        char[] chars = s.toCharArray();
+        for (char c : chars) {
+            stack.push(c);
+        }
+        while (!stack.isEmpty()) {
+            System.out.print(stack.pop());
+        }
+        System.out.println();
     }
 }
