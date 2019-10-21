82641d0bbaf9abd4e725eb588596b0b344e8e259
wbxing <wenbiaoxing@gmail.com>
2019-10-21 21:12:17 +0800

    中缀转后缀表达式 - 测试

--- src/com/test/TestInfixToSuffix.java
+++ src/com/test/TestInfixToSuffix.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 
 public class TestInfixToSuffix {
     public static void main(String[] args) {
+//        String input = "A*(B+C)-D/(E+F)";
         String input;
         System.out.println("Enter infix:");
         Scanner scanner = new Scanner(System.in);
