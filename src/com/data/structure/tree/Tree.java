a6bf4a2fe53664b87f0bf3952fc6accef5f05d11
wbxing <wenbiaoxing@gmail.com>
2019-10-26 14:32:34 +0800

    树的接口

--- src/com/data/structure/tree/Tree.java
+++ src/com/data/structure/tree/Tree.java
@@ -2,17 +2,17 @@ package com.data.structure.tree;
 
 public interface Tree {
     // 查找节点
-    Node find();
+    Node find(int key);
     // 插入节点
-    boolean insert();
+    boolean insert(int data);
     // 删除节点
-    boolean delete();
+    boolean delete(int key);
     // 中序遍历
-    void infixOrder();
+    void infixOrder(Node current);
     // 前序遍历
-    void preOrder();
+    void preOrder(Node current);
     // 后续遍历
-    void postOrder();
+    void postOrder(Node current);
     // 查找最大值
     Node findMax();
     // 查找最小值
