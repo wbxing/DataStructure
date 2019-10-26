45c62c1401abb096a2adbbc1f44e84f361eeca3b
wbxing <wenbiaoxing@gmail.com>
2019-10-26 14:23:32 +0800

    树的节点类

---
+++ src/com/data/structure/tree/Node.java
@@ -0,0 +1,24 @@
+package com.data.structure.tree;
+
+/**
+ * 树的节点
+ */
+public class Node {
+    // 节点数据
+    private int data;
+    // 左子节点
+    private Node leftChildNode;
+    // 右子节点
+    private Node rightChildNode;
+    // 标记节点是否被删除
+    private boolean delete;
+
+    public Node(int data) {
+        this.data = data;
+    }
+
+    // 输出节点
+    public void display() {
+        System.out.println(this.data);
+    }
+}
