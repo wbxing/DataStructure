4decaf827c57a57cff2dac7ea5c41fb82663a571
wbxing <wenbiaoxing@gmail.com>
2019-10-26 14:55:11 +0800

    树的接口

--- src/com/data/structure/tree/Tree.java
+++ src/com/data/structure/tree/Tree.java
@@ -8,11 +8,11 @@ public interface Tree {
     // 删除节点
     boolean delete(int key);
     // 中序遍历
-    void infixOrder(Node current);
+    void infixOrder();
     // 前序遍历
-    void preOrder(Node current);
+    void preOrder();
     // 后续遍历
-    void postOrder(Node current);
+    void postOrder();
     // 查找最大值
     Node findMax();
     // 查找最小值
