0e863d6c86275d6f48aff9e8ce0b208c13f279b3
wbxing <wenbiaoxing@gmail.com>
2019-12-08 22:40:28 +0800

    冒泡排序

---
+++ src/com/data/structure/sort/BubbleSort.java
@@ -0,0 +1,40 @@
+package com.data.structure.sort;
+
+public class BubbleSort {
+    public static void sort (int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            // 设置交换标记
+            boolean flag = true;
+            for (int j = 1; j < array.length - i; j++) {
+                if (array[j - 1] > array[j]) {
+                    int temp = array[j - 1];
+                    array[j - 1] = array[j];
+                    array[j] = temp;
+                    flag = false;
+                }
+            }
+            if (flag) {
+                break;
+            }
+            // 此处可以展示每一轮的中间结果
+            System.out.print("第" + (i + 1) + "轮的结果：");
+            display(array);
+        }
+    }
+
+    public static void display(int[] array) {
+        for (int a : array) {
+            System.out.print(a + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] array = {3, 1, 7, 9, 6, 8, 5, 2, 4};
+        System.out.print("未排序的数组：");
+        display(array);
+        System.out.println("排序：");
+        sort(array);
+        display(array);
+    }
+}
