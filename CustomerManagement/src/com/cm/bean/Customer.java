45f4bf38038754f64c52cf7ce0632d084e497824
wbxing <wenbiaoxing@gmail.com>
2020-01-06 23:07:52 +0800

    Customer 实体类

---
+++ CustomerManagement/src/com/cm/bean/Customer.java
@@ -0,0 +1,63 @@
+package com.cm.bean;
+
+public class Customer {
+
+    private String name;
+    private char gender;
+    private int age;
+    private String phone;
+    private String email;
+
+    public Customer() {
+        // 无参构造默认添加 “张三” 的信息
+        this("张三", '男', 30, "12300000000", "zhangsan@email.com");
+    }
+
+    public Customer(String name, char gender, int age, String phone, String email) {
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
