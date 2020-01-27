011daee2371fc48aabce3b2b2054c84ff82ba6e2
wbxing <wenbiaoxing@gmail.com>
2020-01-27 22:54:22 +0800

    add method "getDetailsForTeam"

--- TeamManagement/src/com/team/domain/Programmer.java
+++ TeamManagement/src/com/team/domain/Programmer.java
@@ -50,4 +50,8 @@ public class Programmer extends Employee {
     public String toString() {
         return getDetails() + "\t程序员\t" + status + "\t\t\t" + equipment.getDescription();
     }
+
+    public String getDetailsForTeam() {
+        return memberId + "/" + getId() + "\t" + getName() + "\t" + getAge() + "\t" + getSalary() + "\t程序员";
+    }
 }
