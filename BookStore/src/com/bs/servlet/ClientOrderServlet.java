2b0ca2cb760d8d037c7da3340e947220e2061e64
wbxing <wenbiaoxing@gmail.com>
2020-04-08 21:44:26 +0800

    修改 manager 路径

--- BookStore/src/com/bs/servlet/ClientOrderServlet.java
+++ BookStore/src/com/bs/servlet/ClientOrderServlet.java
@@ -13,32 +13,8 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
-public class OrderServlet extends BaseServlet {
+public class ClientOrderServlet extends BaseServlet {
     private IOrderService orderService = new OrderServiceImpl();
-
-    protected void createOrder(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Cart cart = (Cart) req.getSession().getAttribute("cart");
-        User user = (User) req.getSession().getAttribute("user");
-        if (user == null) {
-            resp.sendRedirect(req.getContextPath() + "/pages/user/login.jsp");
-        } else {
-            Integer userId = user.getId();
-            String orderId = orderService.createOrder(cart, userId);
-            req.getSession().setAttribute("orderId", orderId);
-            resp.sendRedirect(req.getContextPath() + "/pages/cart/checkout.jsp");
-        }
-    }
-
-    protected void showAllOrder(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        User user = (User) req.getSession().getAttribute("user");
-        if (user == null) {
-            resp.sendRedirect(req.getContextPath() + "/pages/user/login.jsp");
-        } else {
-            List<Order> orders = orderService.showAllOrders();
-            req.setAttribute("AllOrders", orders);
-            req.getRequestDispatcher("/pages/manager/order_manager.jsp").forward(req, resp);
-        }
-    }
 
     protected void sendOrder(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String orderId = req.getParameter("orderId");
